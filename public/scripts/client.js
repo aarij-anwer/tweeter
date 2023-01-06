4ee4726ff165d90e2d2be7662f3d32feaaddb8de
Aarij Anwer <aarij.anwer@gmail.com>
2023-01-06 11:54:59 -0500

    add creatTweetElement function, test it through  console.log

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = function(tweet) {
+  let answer = 
+  `<article>
+    <header>
+      <div>
+        <img src="${tweet.user.avatars}">
+        <p class="name">${tweet.user.name}</p>
+      </div>
+      <p class="handle">${tweet.user.handle}</p>
+    </header>
+    <p class="tweet">${tweet.content.text}</p>
+    <span><hr /></span>
+    <footer>
+      <p class="days">${tweet.created_at}</p>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
+
+  return answer;
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
