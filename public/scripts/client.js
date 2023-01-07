ee92e4e4f6bfa6cc960ef2d2450b9f2ab7486049
Aarij Anwer <aarij.anwer@gmail.com>
2023-01-06 21:03:45 -0500

    add loadTweets function to load tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 // takes an object called `tweet` that represents a tweet
 const createTweetElement = function(tweet) {
   const answer =
@@ -67,9 +40,16 @@ const renderTweets = function(tweets) {
   });
 };
 
+const loadTweets = function() {
+  $.get("/tweets", function(data) {
+    renderTweets(data);
+  });
+};
+
 // renderTweets is called when the document is ready and loaded
 $(document).ready(function() {
-  renderTweets(data);
+
+  loadTweets();
 
   $("#tweet").submit(function(event) {
     $.post("/tweets", $(this).serialize());
